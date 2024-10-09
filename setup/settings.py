a6a0da3d0d0ca58cd9a10137e4a70f986db9b95c
Ricardo Silva <7474175+romstk@users.noreply.github.com>
2024-10-08 21:31:28 -0300

    Configurando as variáveis de ambiente no projeto

--- setup/settings.py
+++ setup/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for setup project.
 
@@ -10,7 +11,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f+7wknulhta=)!$=+xj-+6n-!)xkemq-e0_in9u3x_bhpuh(s!'
+# vou buscar minha SECRET_KEY das minhas variáveis de ambiente do arquivo .env
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
