a07b8444456940b51a7ea1fe8d476d79fb6df30a
Ricardo Silva <7474175+romstk@users.noreply.github.com>
2024-11-20 15:11:29 -0300

    Atualização página receita, para mostrar os detalhes de uma receita. Está mostrando os dados estaticamente de uma única receita que depois nas próximas atualizações faremos atualizaçõs dinâmicas a partir de dados salvos no banco de dados.

--- receitas/views.py
+++ receitas/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 def index (request):
     return render(request, 'receitas/index.html')
 
+
+#Esta function tem o nome da url definida em urls.py(rlpatterns), ao ser chamada a url aciona esta função
 def receita(request): 
     return render(request, 'receitas/receita.html')
